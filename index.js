39927f0acc53929fcd0455dc0e9d92ca1b3078c2
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2023-02-20 22:23:04 -0600

    work on restructure of questions

--- index.js
+++ index.js
@@ -1,109 +1,117 @@
-// const Employee = require('./lib/employee');
-// const Engineer = require('./lib/engineer');
-// const Intern = require('./lib/intern');
-// const Manager = require('./lib/manager');
-// const generateHTML = require('./dist/generatHTML');
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the employee name?',
-            name: 'name',
-            validate: function (input) {
-                if (input === "") {
-                    return 'Please enter employee name.'
-                }
-                return true
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is employee email?',
-            name: 'email',
-            validate: function (input) {
-                if (input === "") {
-                    return 'Please enter employee email.'
-                }
-                return true
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is employee ID#?',
-            name: 'ID',
-            validate: function (input) {
+var emplSelect = {
+    //Used to determine which eployee selected
+    type: 'list',
+    message: 'What is employee classification?',
+    name: 'employee',
+    choices: ['Intern', 'Engineer', 'Manager']
+}
 
-                if (isNaN(parseInt(input))) {
-                    return 'Please enter # for employee ID#'
-                }
-                return true
-            }
-        },
-        {//how can I use this question to break the seperate questions apart determined by employee?
-            type: 'list',
-            message: 'What is employee classification?',
-            name: 'employee',
-            choices: ['Intern', 'Engineer', 'Manager']
-        },
-
-    ])
-    .then((data) => {
-        console.log(JSON.stringify(data));
-        if (data.employee === 'Manager') {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        message: 'What is the Office number?',
-                        name: 'officeNumber',
-                        validate: function (input) {
-                            //can I validate by checking for interger here if so how so?
-                            if (isNaN(parseInt(input))) {
-                                return 'Please enter office number.'
-                            }
-                            return true
-                        }
-                    }
-                ])
-        } else if (data.employee === 'Intern') {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-            message: 'What school does intern attend?',
-            name: 'school',
-            validate: function (input) {
-                //can I validate by checking for interger here if so how so?
-                if (input === "") {
-                    return 'Please enter school name.'
-                }
-                return true
-            }
-                    }
-                ])
-        } else if (data.employee === 'Engineer') {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        message: 'What is employee Github username?',
-                        name: 'username',
-                        //validation code
-                        validate: function (input) {
-                            if (input === "") {
-                                return 'Please enter the Github username!'
-                            }
-                            return true
-                        }
-                    }
-                ])
+var emplAllQuestions = [{
+    type: 'input',
+    message: 'What is the employee name?',
+    name: 'name',
+    validate: function (input) {
+        if (input === "") {
+            return 'Please enter employee name.'
         }
+        return true
+    }
+},
+{
+    type: 'input',
+    message: 'What is employee email?',
+    name: 'email',
+    validate: function (input) {
+        if (input === "") {
+            return 'Please enter employee email.'
+        }
+        return true
+    }
+},
+{
+    type: 'input',
+    message: 'What is employee ID#?',
+    name: 'ID',
+    validate: function (input) {
 
+        if (isNaN(parseInt(input))) {
+            return 'Please enter # for employee ID#'
+        }
+        return true
+    }
+}
+]
+
+var emplManager = () => {
+    emplAllQuestions.push({
+        type: 'input',
+        message: 'What is the Office number?',
+        name: 'officeNumber',
+        validate: function (input) {
+            //can I validate by checking for interger here if so how so?
+            if (isNaN(parseInt(input))) {
+                return 'Please enter office number.'
+            }
+            return true
+        }
     })
-    .then((data) => {
-        console.log(JSON.stringify(data));
-    });
+}
+
+var emplEngin = () => {
+    emplAllQuestions.push({
+        type: 'input',
+        message: 'What is employee Github username?',
+        name: 'username',
+        //validation code
+        validate: function (input) {
+            if (input === "") {
+                return 'Please enter the Github username!'
+            }
+            return true
+        }
+    })
+}
+
+var emplIntern = () => {
+
+    emplAllQuestions.push({
+        type: 'input',
+        message: 'What school does intern attend?',
+        name: 'school',
+        validate: function (input) {
+            //can I validate by checking for interger here if so how so?
+            if (input === "") {
+                return 'Please enter school name.'
+            }
+            return true
+        }
+    })
+}
+
+var init = () => {
+    return (
+        inquirer
+            .prompt(emplSelect)
+            .then((choice) => {
+                if (choice.employee === 'Manager') {
+                    emplManager()
+                } else if (choice.employee === 'Engineer') {
+                    emplEngin()
+                } else {
+                    emplIntern()
+                }
+
+
+            })
+            .then((data) => {
+                console.log(JSON.stringify(data));
+            })
+    )
+}
+
+init();
